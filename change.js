f4f1d3cffaef539aaca2920b739be658fe29e075
Bao <4552599538@qq.com>
2020-05-29 15:10:25 -0700

    add documentation

--- change.js
+++ change.js
@@ -1,4 +1,12 @@
-
+/**
+ * getChange accepts two parameters (totalPayable and cashPaid) and calculates
+ * the change in "coins" that needs to be returned.
+ * @param {number} totalPayable the integer amount (in pennies) to be paid
+ * @param {number} cashPaid the integer amount (in pennies) the person paid
+ * @returns {array} list of coins we need to dispense to the person as change
+ * @example
+ * getChange(215, 300); // returns [50, 20, 10, 5]
+ */
 var getChange = function (totalPayable, cashPaid) {
     'use strict';
 
